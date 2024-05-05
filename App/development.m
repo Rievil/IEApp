95ffa5d7a68166bf4fff00792d400443a5e37ec4
Richard Dvořák <48693908+Rievil@users.noreply.github.com>
2024-05-05 09:57:17 +0200

    Added MKSQlite submodule from git

--- App/development.m
+++ App/development.m
@@ -1,5 +1,10 @@
 addons = matlab.addons.installedAddons;
-writetable(addons,'Addons_after.xlsx');
-
+% writetable(addons,'Addons_after.xlsx');
 %%
+ss=ver;
+%%
+
+needed=["data_acq_toolbox","matlab","signal_toolbox"];
+%https://www.mathworks.com/matlabcentral/fileexchange/45171-data-acquisition-toolbox-support-package-for-windows-sound-cards
+
 
